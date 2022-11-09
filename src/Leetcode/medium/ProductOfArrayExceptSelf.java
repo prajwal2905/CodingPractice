7aba936139f8fbb7a5a6c655d0b314ab22ec6f55
Prajwal Jayaprakash <58502201+prajwal2905@users.noreply.github.com>
2022-11-08 20:30:03 -0800

    Update ProductOfArrayExceptSelf.java

--- src/Leetcode/medium/ProductOfArrayExceptSelf.java
+++ src/Leetcode/medium/ProductOfArrayExceptSelf.java
@@ -1,4 +1,13 @@
 class ProductOfArrayExceptSelf {
+//[1,2,3,4]
+//Array can be positive, negative or zeros
+        //Three ways to solve O(n2) - > Go over array calculate product of all elements except self and add to output
+        // O(n) with space -> Have two arrays pre and post add post multiple of each element in post array and pre multiple of each element in second and finally multiply each element in both arrays
+        // O(n) with temp variable -> have a temp variable assigned to 1 iterate once with calculating pre miltiples in output, next calculate post and multiply using the output array.
+        
+// Pre [1, 1, 2, 6]
+//Post [24, 12, 4, 1]
+//REsult [24, 12, 8, 6]
 public int[] productExceptSelf(int[] nums) {
         
         int temp = 1;
